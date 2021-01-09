29741f3a1c97e1a7cb8acf014abba8769e0c51bb
Andrew <khenkinandrew@gmail.com>
2021-01-09 12:29:08 -0500

    Objects and arrays

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,16 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+//Arrays
+var names = ['luigi', 'mario', 'luigi'];
+names.push('toad');
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+// Objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
 };
-console.log(circ(10));
+console.log(ninja);
--- sandbox.ts
+++ sandbox.ts
@@ -1,10 +1,26 @@
-let character = 'mario';
-let age=30;
-let isBlackBelt = false;
+//Arrays
+
+let names = ['luigi', 'mario', 'luigi'];
+
+names.push('toad');
+
+let numbers = [10, 20, 30, 40];
+
+numbers.push(25);
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
 
 
-const circ = (diameter: number) => {
-    return diameter * Math.PI;
-}
+// Objects
 
-console.log(circ(10))
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+
+console.log(ninja);
