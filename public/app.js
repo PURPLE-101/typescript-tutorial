6f09f2c417dfc850a155280eaf5b18209aff310b
Andrew <khenkinandrew@gmail.com>
2021-01-12 21:58:52 -0500

    Modules

--- public/app.js
+++ public/app.js
@@ -1,40 +1,20 @@
-"use strict";
-// classes
-var Invoice = /** @class */ (function () {
-    // readonly client: string;
-    // private details: string;
-    // public amount: number;
-    // constructor(c: string, d: string, a: number){
-    //     this.client = c;
-    //     this.details = d;
-    //     this.amount = a;
-    // }
-    function Invoice(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    Invoice.prototype.format = function () {
-        return this.client + " owes $" + this.amount + " for " + this.details;
-    };
-    return Invoice;
-}());
-var invOne = new Invoice('mario', 'work on the mario website', 250);
-var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-var invoices = [];
+import { Invoice } from './classes/Invoice.js';
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 invTwo.amount = 400;
-invoices.forEach(function (inv) {
+invoices.forEach(inv => {
     console.log(inv.client, inv.amount, inv.format());
 });
-var form = document.querySelector('.new-item-form');
+const form = document.querySelector('.new-item-form');
 //inputs
-var type = document.querySelector('#type');
-var toFrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
+const type = document.querySelector('#type');
+const toFrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
 });
