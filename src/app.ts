17294d83ba157d28501d7ba2559ccd125e8dbbbd
Andrew <khenkinandrew@gmail.com>
2021-01-12 21:35:55 -0500

    Classes in typescript

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,33 @@
-// ! means we know for sure that we will get some type of value
-// const anchor = document.querySelector('a')!;
+// classes
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
 
-// console.log(anchor);
+    constructor(c: string, d: string, a: number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+
+console.log(invoices);
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-//console.log(form.children);
 
 //inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
