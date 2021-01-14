8e39b5819c2fc1f9869ed8682b204f4f9d0da5a5
Andrew <khenkinandrew@gmail.com>
2021-01-13 19:09:19 -0500

    Generics

--- src/app.ts
+++ src/app.ts
@@ -28,3 +28,36 @@ form.addEventListener('submit', (e: Event) => {
 
     list.render(doc, type.value, 'end');
 });
+
+// generics
+
+const addUID = <T extends {name: string}>(obj: T) => {
+    let uid = Math.floor(Math.random() * 100);
+    return {...obj, uid};
+}
+
+let docOne = addUID({name: 'yoshi', age: 40});
+// let docTwo = addUID('hello');
+
+console.log(docOne);
+
+// with interfaces
+interface Resource<T> {
+    uid: number;
+    resourceName: string;
+    data: T;
+}
+
+const docThree: Resource<object> = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'Shaun'}
+}
+
+const docFour: Resource<string[]> = {
+    uid: 2,
+    resourceName: 'shoppingList',
+    data: [ 'bread', 'milk', 'water']
+}
+
+console.log(docThree, docFour);
